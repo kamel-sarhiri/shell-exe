c2c66f774a724c21274c71149220168217c07cdd
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-09-28 16:07:49 +0200

    Job 3 réalisé

---
+++ job3/add.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Calculer la somme via des arguments de ligne de commande
+
+# $1 et $2 font référence au premier et au deuxième argument passés en tant qu’arguments de ligne de commande
+
+somme=$(( $1 + $2 ))
+
+echo « La somme est : $somme »
