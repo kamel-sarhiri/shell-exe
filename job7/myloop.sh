8fc900f04b88ffa2e5a717375684b3b1e33311ee
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-09-29 10:30:01 +0200

    MAJ myloop.sh avec deuxième méthode

--- job7/myloop.sh
+++ job7/myloop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-for i in {1..10}
-do 
-	echo "Je suis un script qui arrive à faire une boucle" $i
+for ((a=1 ; a <= 10 ; a++))
+do 	
+	echo "Je suis un script qui arrive à faire une boucle" $a
 done
+
+
+# il existe une autre méthode qui fonctionne aussi
+#!/bin/bash
+#for i in {1..10}
+#do 
+#	echo "Je suis un script qui arrive à faire une boucle" $i
+#done
