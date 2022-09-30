6752f6b6c80ceadd3655d2c186384476a0b6147c
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-09-30 16:55:25 +0200

    Job 9 réalisé en grande partie

---
+++ job9/accessrights.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd ~/shell-exe/job9
+
+cat home/kamel/shell-exe/job9/Shell_Userlist.csv | while read varligne
+do
+	password=`echo $varligne |cut -d ',' -f4`
+	username=`echo $varligne | cut -d ',' -f2`
+	username=`echo ${username,,}`
+	role=`echo $varligne |cut -d ',' -f5` 
+	echo $role
+	if [ ${role:0:5} = "Admin" ]
+	then
+		echo "creation de l'utilisateur : $username"
+		sudo useradd -m -$username -p $password
+		echo "changement de rôle de : $username"
+		sudo usermod -aG sudo $username
+
+	else
+	
+		echo "creation de l'utilisateur : $username"
+		sudo useradd -m $username -p $password
+
+	fi		
+done < <(tail -n +2 Shell_Userlist.csv)
